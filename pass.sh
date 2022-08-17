61367db3dc0599e168be81e5bae958c8308dc692
Dian Pamilih <dian.pamilih@elastic.co>
2022-08-17 13:39:59 +1000

    added oneoff deployment and rearrange directory

--- pass.sh
+++ pass.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export ES_PASSWORD=$(kubectl -n elastic-system get secret elasticsearch-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
+export ES_PASSWORD=$(kubectl -n $1 get secret elasticsearch-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
 echo $ES_PASSWORD
